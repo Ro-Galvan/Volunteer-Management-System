ca834dd09fa64be745523922139fd4104406713b
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-07-24 15:10:24 -0400

    first commit

---
+++ src/main/java/org/vms/volunteer/VolunteerApplication.java
@@ -0,0 +1,13 @@
+package org.vms.volunteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vms/volunteer/VolunteerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vms.volunteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
