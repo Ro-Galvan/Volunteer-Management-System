0e3c5454f1d2242169cb5c3e4a39ff0c8693572b
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-07-30 14:42:16 -0400

    all tests pass for Nonprofit

--- src/test/java/org/vms/volunteer/dao/NonprofitDaoDBTest.java
+++ src/test/java/org/vms/volunteer/dao/NonprofitDaoDBTest.java
@@ -60,13 +60,62 @@ class NonprofitDaoDBTest {
 
     @Test
     void getAllNonprofits() {
+        // Arrange - Add two nonprofits to the database
+        Nonprofit nonprofit1 = new Nonprofit("Tech4Good", "757-555-1234", "test1@test.com", "321 Pine Blvd, Raleigh, NC", "Using technology for social impact");
+        Nonprofit nonprofit2 = new Nonprofit("Charity Aid", "919-555-5678", "test2@test.com", "123 Oak St, Raleigh, NC", "Providing aid to those in need");
+
+        nonprofit1 = nonprofitDao.addNonprofit(nonprofit1);
+        nonprofit2 = nonprofitDao.addNonprofit(nonprofit2);
+
+        // Act - Call the getAllNonprofits() method
+        List<Nonprofit> allNonprofits = nonprofitDao.getAllNonprofits();
+
+        // Assert
+        assertNotNull(allNonprofits);
+        assertEquals(2, allNonprofits.size()); // Assuming there are two test nonprofits in the database
+        assertEquals("Tech4Good", allNonprofits.get(0).getCompanyName());
+        assertEquals("Charity Aid", allNonprofits.get(1).getCompanyName());
     }
+
 
     @Test
     void updateNonprofit() {
+        // Arrange - add a nonprofit to the database
+        Nonprofit nonprofit = new Nonprofit("Tech4Good", "757-555-1234", "test@test.com", "321 Pine Blvd, Raleigh, NC", "Using technology for social impact");
+        nonprofit = nonprofitDao.addNonprofit(nonprofit);
+
+        // Modify the nonprofit object
+        nonprofit.setCompanyName("Updated Company Name");
+        nonprofit.setPhoneNumber("555-555-5555");
+        nonprofit.setEmail("updated@test.com");
+        nonprofit.setAddress("123 Oak St, Raleigh, NC");
+        nonprofit.setMission("Updated mission statement");
+
+        // Act - Call the updateNonprofit() method
+        nonprofitDao.updateNonprofit(nonprofit);
+
+        // Assert - Get the updated nonprofit by ID and verify the changes
+        Nonprofit updatedNonprofit = nonprofitDao.getNonprofitByID(nonprofit.getId());
+        assertNotNull(updatedNonprofit);
+        assertEquals("Updated Company Name", updatedNonprofit.getCompanyName());
+        assertEquals("555-555-5555", updatedNonprofit.getPhoneNumber());
+        assertEquals("updated@test.com", updatedNonprofit.getEmail());
+        assertEquals("123 Oak St, Raleigh, NC", updatedNonprofit.getAddress());
+        assertEquals("Updated mission statement", updatedNonprofit.getMission());
     }
 
     @Test
     void deleteNonprofitByID() {
+        // Arrange - add a nonprofit to the database
+        Nonprofit nonprofit = new Nonprofit("Tech4Good", "757-555-1234", "test@test.com", "321 Pine Blvd, Raleigh, NC", "Using technology for social impact");
+        nonprofit = nonprofitDao.addNonprofit(nonprofit);
+
+        // Act - Call the deleteNonprofitByID() method
+        nonprofitDao.deleteNonprofitByID(nonprofit.getId());
+
+        // Assert - Try to get the deleted nonprofit by ID, it should return null
+        Nonprofit deletedNonprofit = nonprofitDao.getNonprofitByID(nonprofit.getId());
+        assertNull(deletedNonprofit);
     }
+
 }
